37c24970050a92027a4684b229a9647735f2154a
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-04-30 15:44:22 +0200

    M-2 added cycle for and print with js inside html

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 console.log("ciao giuseppe")
+
+let grid = document.getElementById("grid")
+//CREO CICLO FOR E FACCIO PARTIRE IL CONTATORE DA 1 E LO FACCUIO FINIRE A 100
+for (let i = 1; i <= 100; i++) {
+    console.log(i)
+    let square = document.createElement("div")
+    square.classList.add("square")
+    square.innerText= i
+    grid.append(square)
+}
